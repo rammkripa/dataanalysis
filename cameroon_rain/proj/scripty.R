04ba1aa372d46c7eac97c19d7bcf4bcf0a070501
Ram Kripa <ramkripa@Rams-Air.lan>
2020-07-02 13:51:20 +0100

    cam updated

--- cameroon_rain/proj/scripty.R
+++ cameroon_rain/proj/scripty.R
@@ -1,21 +1,26 @@
 library(tidyverse)
+library(lubridate)
 dat = read.csv("CM000004910.csv")
 glimpse(dat)
 tidied_dat = dat %>%
-  separate(col=DATE,into=c("Year","Month","Day"))
-by_month = tidied_dat %>%
-  group_by(Month)
+  mutate(DATE=ymd(DATE)) %>%
+  mutate(month=month(DATE),year = year(DATE))
+  
 
 jja <- tidied_dat %>%
-  filter((Month=='06'|Month=='07'|Month=='08'))
-summ <- jja %>%
-  filter(Year>=1976 & Year<2020)%>%
-  group_by(Year)%>%
-  filter(PRCP<=3)%>%
+  filter((month>=6)&(month<=8))
+jto <- tidied_dat %>%
+  filter((month>=6)&(month<=10))
+viz_breaks = function(df,lev,nam){
+summ <- df %>%
+  filter(year>=1976 & year<2020)%>%
+  group_by(year)%>%
+  filter(PRCP<=lev)%>%
   summarize(count=n())
-p <- ggplot(summ,aes(x=Year,y=count))+
-  geom_point()
-spline.d <- as.data.frame(spline(summ$Year, summ$count))
-p + geom_line(data = spline.d, aes(x = x, y = y))
+spline.d <- as.data.frame(spline(summ$year, summ$count))
 ggplot(data = spline.d, aes(x = x, y = y))+
-  geom_line()
+  geom_line()+
+  labs(title=nam)
+}
+viz_breaks(jja,3,"jja breaks")
+viz_breaks(jto,3,"jto breaks")
