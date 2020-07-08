1848d2831bc29b9e83e3f73bf491666d41c988e8
Ram Kripa <ramkripa@Rams-Air.lan>
2020-07-08 15:56:00 +0100

    rmd added cameroon

--- cameroon_rain/proj/scripty.R
+++ cameroon_rain/proj/scripty.R
@@ -1,6 +1,8 @@
+
 library(tidyverse)
 library(lubridate)
-dat = read.csv("CM000004910.csv")
+
+dat = read_csv("data/CM000004910.csv")
 glimpse(dat)
 tidied_dat = dat %>%
   mutate(DATE=ymd(DATE)) %>%
@@ -8,19 +10,32 @@ tidied_dat = dat %>%
   
 
 jja <- tidied_dat %>%
-  filter((month>=6)&(month<=8))
+  filter((month>=6)&(month<=8),year>=1976 & year<2020)
 jto <- tidied_dat %>%
   filter((month>=6)&(month<=10))
-viz_breaks = function(df,lev,nam){
-summ <- df %>%
-  filter(year>=1976 & year<2020)%>%
-  group_by(year)%>%
-  filter(PRCP<=lev)%>%
-  summarize(count=n())
-spline.d <- as.data.frame(spline(summ$year, summ$count))
-ggplot(data = spline.d, aes(x = x, y = y))+
-  geom_line()+
-  labs(title=nam)
-}
-viz_breaks(jja,3,"jja breaks")
-viz_breaks(jto,3,"jto breaks")
+
+jjaresults = jja %>%
+  group_by(year) %>%
+  summarize(total=sum(PRCP,na.rm=TRUE),num_dry=sum(PRCP<=3,na.rm=TRUE)) %>%
+  filter(total!=0)
+ggplot(jjaresults,mapping=aes(x=total))+
+  geom_histogram(bins=10)
+
+ggplot(jjaresults,mapping=aes(x=total,y=num_dry))+
+  geom_point()+
+  geom_smooth()
+
+(1976+2019)/2
+
+jjaresults %>%
+  mutate(type=if_else(year<1997,'before 1997','after 1997')) %>%
+ggplot(mapping=aes(x=total,color=type))+
+  geom_freqpoly(bins=10)
+
+
+
+ggplot(jjaresults,mapping=aes(x=year,y=num_dry))+
+  geom_line()
+
+  
+  
